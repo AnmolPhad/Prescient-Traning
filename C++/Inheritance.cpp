d4c41d933ba703ff9371d64038672696f5768154
AnmolPhad <120477856+AnmolPhad@users.noreply.github.com>
2026-09-17 08:17:18 +0530

    Refactor code structure for improved readability and maintainability

--- C++/Inheritance.cpp
+++ C++/Inheritance.cpp
@@ -5,17 +5,65 @@
 using namespace std;
 
 
-class person {
+class Person {
 
+protected:
     string name;
     int age;
 
-
-    person()
+public :
+    Person()
     {
-        cout << "Base Constructor";
+        cout << "Base Constructor ";
         name = "";
         age = 0;
     }
+
+    Person(string n, int a){
+        cout << "\nParameterized Constructor of base class";
+        name = n;
+        age = a;
+    }
+
+   virtual void display(){
+        cout << "\nName: " << name << endl;
+        cout << "Age: " << age << endl;
+    }
     
 };
+
+class Student : public Person
+{
+    int roll;
+public :
+    Student() 
+    {
+        cout << "\nDerived Constructor";
+        roll = 0;
+    }
+
+    Student(string n, int a, int r) : Person(n, a)
+    {
+        cout << "\nParameterized Constructor of derived class";
+        roll = r;
+    }
+    void display() override {
+        Person::display();
+        cout << "\nRoll: " << roll << endl;
+    }
+
+};
+
+int main(void){
+Person* p1 =new  Student("Rahul", 20, 101);
+
+p1->display();
+cout << typeid(p1).name();
+
+Student* s1 =dynamic_cast<Student*>(p1);
+
+s1->display();
+
+return 0;
+
+}
